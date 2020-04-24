22266cd0d1d2723c68bf0651bc224f3fe0e72d57
Mirko Tomovic <mirko.mire.tomovic@gmail.com>
2020-04-24 10:14:32 +0200

    BP-13 - Added basic gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,26 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        /**
+         * Define Gate for admin user role
+         * Returns true if user role is set to admin
+         **/ 
+        Gate::define('isAdmin', function($user) {
+            return $user->role == 'admin';
+        });
+        /**
+         * Define Gate for salesman user role
+         * Returns true if user role is set to salesman
+         **/ 
+        Gate::define('isSalesman', function($user) {
+            return $user->role == 'salesman';
+        });
+        /**
+         * Define Gate for professor user role
+         * Returns true if user role is set to professor
+         **/ 
+        Gate::define('isCustomer', function($user) {
+            return $user->role == 'customer';
+        });
     }
 }
