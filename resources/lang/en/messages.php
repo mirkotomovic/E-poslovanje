da85254463f265711ab0cc72791660eb0703adfb
Savo <Vukovic23@hotmail.com>
2020-04-24 23:37:28 +0200

    BP-5 Design pages that hold key information about the application/developers

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -10,5 +10,7 @@ return [
     */
 
     'main-text' => "We represent a company that provides a solution for online purchases of traveling tickets. You can imagine our company as a middle man between you and a traveling company, we're here to ensure you know exactly where you're going and for what cost.",
-
+    'ticket-system' => "Purchase any ticket with just a few clicks.",
+    'search-system' => "Save time by comparing all your travel options in one place.",
+    'support-system' => "Our world class team of bus experts is always here to help.",
 ];
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,5 +11,28 @@
 @section('container')    
     <div class="mx-auto w-25">
         <p class="text-center font-weight-normal text-justify">@lang('messages.main-text')</p>
+    </div>    
+    <hr class="my-3">
+    <h5 class="font-italic text-center"><span class="badge badge-secondary text-primary">We offer</span></h5>
+    <hr class="my-4">
+    <div class="row mb-5">
+        <div class="col-4 d-flex justify-content-center">
+            <div class="mb-2 w-25 h-30">
+                <img class="img-thumbnail" src="{{asset('img/tickets.png')}}" alt="tickets">
+                <p class="font-light text-center text-justify">@lang('messages.ticket-system')</p>
+            </div>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+            <div class="mb-2 w-25 h-30">
+                <img class="img-thumbnail" src="{{asset('img/bus-time.png')}}" alt="bus-time">
+                <p class="font-light text-center text-justify">@lang('messages.search-system')</p>
+            </div>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+            <div class="mb-2 w-25 h-30">
+                <img class="img-thumbnail" src="{{asset('img/support.png')}}" alt="support">
+                <p class="font-light text-center text-justify">@lang('messages.support-system')</p>
+            </div>
+        </div>
     </div>
 @endsection
