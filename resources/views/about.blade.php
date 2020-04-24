b626d84211a9ccdcda4ff625f70572102a43d11e
Savo <Vukovic23@hotmail.com>
2020-04-24 18:02:41 +0200

    additional work on the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.base')
 
-@section('container')
-    <div class="jumbotron color-primary">
+@section('top-body-elements')
+    <div class="jumbotron">
         <h1 class="display-3 text-center">{{config('app.name','Bus Ticketing')}}</h1>
         <p class="lead text-center">Secure your travel expenses from the comfort of your home</p>
         <hr class="my-4">
-        <p class="text-center">@lang('messages.main-text')</p>
-    </div>    
+    </div>
+@endsection
+
+@section('container')    
+    <div class="mx-auto w-25">
+        <p class="text-center font-weight-normal text-justify">@lang('messages.main-text')</p>
+    </div>
 @endsection
