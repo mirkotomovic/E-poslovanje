bc6d7b23c8158be6419aa16b8c9636aee478a193
Savo <Vukovic23@hotmail.com>
2020-04-24 15:03:08 +0200

    removed the additional login link from the navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,9 +16,6 @@
       <li class="nav-item">
         <a class="nav-link" href="\contact">Contact</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="\login">Login</a>
-      </li>
     </ul>
     
      <!-- Right Side Of Navbar -->
