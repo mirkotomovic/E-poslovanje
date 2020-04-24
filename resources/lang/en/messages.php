5a9899f158a4d705914efd6c7071dbd7170c38d7
Nikola Kilibarda <k.nikola.lrs@gmail.com>
2020-04-24 18:16:05 +0200

    Fixed spelling of a few words in about page text.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -9,6 +9,6 @@ return [
     |
     */
 
-    'main-text' => "We represent a company that provides a solution for online purchaces of traveling tickets. You can imagine our company as a middle man between you and a traveling company, we're here to ensure you know exactly where're your going and for what cost.",
+    'main-text' => "We represent a company that provides a solution for online purchases of traveling tickets. You can imagine our company as a middle man between you and a traveling company, we're here to ensure you know exactly where you're going and for what cost.",
 
 ];
