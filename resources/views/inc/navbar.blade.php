0a42bc971137f1d23bcfe5e2332dd403dad181bc
Mirko Tomović <mirko.mire.tomovic@gmail.com>
2020-08-25 21:35:52 +0200

    added create company to admin navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,96 +1,109 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-  <div class="container">
-      <a class="navbar-brand" href="{{ route('home') }}">
-          <img class="logo-size" src="{{ asset('img/logo.svg') }}" alt="logo" height="50">
-          {{config('app.name', 'Bus Ticketing')}}
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-          aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img class="logo-size" src="{{ asset('img/logo.svg') }}" alt="logo" height="50">
+            {{config('app.name', 'Bus Ticketing')}}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+            aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          @can('isAdmin')
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('places.create') }}">{{ __('Create Places') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('paths.create') }}">{{ __('Create Paths') }}</a>
-              </li>
-          </ul>
-          @elsecan('isCustomer', $post)
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
-              </li>
-          </ul>
-          @elsecan('isCustomer', $post)
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
-              </li>
-          </ul>
-          @endcan
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            @can('isAdmin')
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('places.create') }}">{{ __('Create Places') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('paths.create') }}">{{ __('Create Paths') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('companies.create') }}">{{ __('Create Company') }}</a>
+                </li>
+            </ul>
+            @elsecan('isCustomer')
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
+                </li>
+            </ul>
+            @elsecan('isCustomer')
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
+                </li>
+            </ul>
+            @else
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
+                </li>
+            </ul>
+            @endcan
 
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-              <!-- Authentication Links -->
-              @guest
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-              </li>
-              @if (Route::has('register'))
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-              </li>
-              @endif
-              @else
-              <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                      {{ Auth::user()->name }} <span class="caret"></span>
-                  </a>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
 
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                            document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                      </a>
+                            {{ __('Logout') }}
+                        </a>
 
-                      <a class="dropdown-item"
-                          href="{{ route('users.show', ['user' => Auth::user() ]) }}">Profile</a>
+                        <a class="dropdown-item" href="{{ route('users.show', ['user' => Auth::user() ]) }}">Profile</a>
 
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                          style="display: none;">
-                          @csrf
-                      </form>
-                  </div>
-              </li>
-              @endguest
-          </ul>
-      </div>
-  </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
 </nav>
