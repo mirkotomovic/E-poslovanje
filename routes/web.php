a7bba469cca5358c73515758321f394a1a4695bc
Nikola Kilibarda <k.nikola.lrs@gmail.com>
2020-08-28 08:18:44 +0200

    Added CRUD routes for all of the admin managed models.

--- routes/web.php
+++ routes/web.php
@@ -23,22 +23,39 @@ Auth::routes();
 
 Route::resource('users', 'UserController');
 
-Route::get('/admin/places', 'PlaceController@index')->name('places.index');
-Route::get('/admin/places/{place}/edit', 'PlaceController@edit')->name('places.edit');
-Route::patch('/admin/places/{place}', 'PlaceController@update')->name('places.update');
-Route::delete('/admin/places/{place}', 'PlaceController@destroy')->name('places.destroy');
 
 
 Route::group(['middleware' => 'auth'], function () {
     Route::middleware('can:isAdmin')->group(function () {
+        Route::get('/admin/places', 'PlaceController@index')->name('places.index');
         Route::get('/admin/places/create', 'PlaceController@create')->name('places.create');
         Route::post('/admin/places/store', 'PlaceController@store')->name('places.store');
+        Route::get('/admin/places/{place}/edit', 'PlaceController@edit')->name('places.edit');
+        Route::patch('/admin/places/{place}', 'PlaceController@update')->name('places.update');
+        Route::delete('/admin/places/{place}', 'PlaceController@destroy')->name('places.destroy');
+
+        Route::get('/admin/paths', 'PathController@index')->name('paths.index');
         Route::get('/admin/paths/create', 'PathController@create')->name('paths.create');
         Route::post('/admin/paths/store', 'PathController@store')->name('paths.store');
+        Route::get('/admin/paths/{path}/edit', 'PathController@edit')->name('paths.edit');
+        Route::patch('/admin/paths/{path}', 'PathController@update')->name('paths.update');
+        Route::delete('/admin/paths/{path}', 'PathController@destroy')->name('paths.destroy');
+
+        Route::get('/admin/companies', 'CompanyController@index')->name('companies.index');
         Route::get('/admin/companies/create', 'CompanyController@create')->name('companies.create');
         Route::post('/admin/companies/store', 'CompanyController@store')->name('companies.store');
+        Route::get('/admin/companies/{company}/edit', 'CompanyController@edit')->name('companies.edit');
+        Route::patch('/admin/companies/{company}', 'CompanyController@update')->name('companies.update');
+        Route::delete('/admin/companies/{company}', 'CompanyController@destroy')->name('companies.destroy');
+
+        Route::get('/admin/journeys', 'JourneyController@index')->name('journeys.index');
         Route::get('/admin/journeys/create', 'JourneyController@create')->name('journeys.create');
         Route::post('/admin/journeys/store', 'JourneyController@store')->name('journeys.store');
+        Route::get('/admin/journeys/{journey}/edit', 'JourneyController@edit')->name('journeys.edit');
+        Route::patch('/admin/journeys/{journey}', 'JourneyController@update')->name('journeys.update');
+        Route::delete('/admin/journeys/{journey}', 'JourneyController@destroy')->name('journeys.destroy');
+
+
     });
     Route::middleware('can:isSalesman')->group(function () {
         
