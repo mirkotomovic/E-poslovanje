4bab857702e7b61d3571d8a75866bf214e03d188
Nikola Kilibarda <k.nikola.lrs@gmail.com>
2020-08-28 08:05:08 +0200

    Changed routes to use proper URL format.

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,14 @@ Route::delete('/admin/places/{place}', 'PlaceController@destroy')->name('places.
 
 Route::group(['middleware' => 'auth'], function () {
     Route::middleware('can:isAdmin')->group(function () {
-        Route::get('/admin/placeCreate', 'PlaceController@create')->name('places.create');
-        Route::post('/admin/placeStore', 'PlaceController@store')->name('places.store');
-        Route::get('/admin/pathCreate', 'PathController@create')->name('paths.create');
-        Route::post('/admin/pathStore', 'PathController@store')->name('paths.store');
-        Route::get('/admin/companyCreate', 'CompanyController@create')->name('companies.create');
-        Route::post('/admin/companyStore', 'CompanyController@store')->name('companies.store');
-        Route::get('/admin/journeyCreate', 'JourneyController@create')->name('journeys.create');
-        Route::post('/admin/journeyStore', 'JourneyController@store')->name('journeys.store');
+        Route::get('/admin/places/create', 'PlaceController@create')->name('places.create');
+        Route::post('/admin/places/store', 'PlaceController@store')->name('places.store');
+        Route::get('/admin/paths/create', 'PathController@create')->name('paths.create');
+        Route::post('/admin/paths/store', 'PathController@store')->name('paths.store');
+        Route::get('/admin/companies/create', 'CompanyController@create')->name('companies.create');
+        Route::post('/admin/companies/store', 'CompanyController@store')->name('companies.store');
+        Route::get('/admin/journeys/create', 'JourneyController@create')->name('journeys.create');
+        Route::post('/admin/journeys/store', 'JourneyController@store')->name('journeys.store');
     });
     Route::middleware('can:isSalesman')->group(function () {
         
